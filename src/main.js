c98db5bd2b54582d93a0562c85566f487369b373
feinno <guowenhuan_2008@126.com>
2019-04-27 08:23:19 +0800

    初始化相关样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 初始化页面样式
+import './assets/styles/reset.css'
+// 解决移动端1px问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
