c32b2ade3f5d6463997932bf1e5ff0027517adc2
feinno <guowenhuan_2008@126.com>
2019-04-27 09:18:49 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import router from './router'
 import './assets/styles/reset.css'
 // 解决移动端1px问题
 import './assets/styles/border.css'
+//解决部分机型点击300ms延迟
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import List from '@/pages/list/List'
 import Home from '@/pages/home/HomePage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/list',
-      name: 'List',
-      component: List
-    }
-  ]
+    path: '/',
+    name: 'Home',
+    component: Home
+  }]
 })
