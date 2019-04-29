69c048d5c92837fd44d08fd3232bd5d2b077d7bb
feinno <guowenhuan_2008@126.com>
2019-04-29 13:36:39 +0800

    轮播图swiper

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 初始化页面样式
 import './assets/styles/reset.css'
 // 解决移动端1px问题
 import 'styles/border.css'
 import 'styles/iconfont.css'
+//使用轮播图插件
+import 'swiper/dist/css/swiper.css'
 //解决部分机型点击300ms延迟
 import fastClick from 'fastclick'
-
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
-
 fastClick.attach(document.body);
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<!--轮播图插件-->
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        //图片显示分页符号
+        pagination: ".swiper-pagination",
+        loop: true,
+        autoplay: 3000
+      },
+      swiperList: [
+        {
+          id: "001",
+          imgUrl: "https://img1.qunarzz.com/qs/1903/ef/f0ec348a8a7c5b02.jpg"
+        },
+        {
+          id: "002",
+          imgUrl: "https://img1.qunarzz.com/qs/1903/5d/9976906642666a02.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang='stylus' scoped>
+#穿透样式scoped限制, .wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 54%;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
