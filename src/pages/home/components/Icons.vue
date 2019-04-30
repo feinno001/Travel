32846a552baf36894c47d610786fd6ccc8322350
feinno <guowenhuan_2008@126.com>
2019-04-30 11:47:45 +0800

    完成icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,12 +1,16 @@
 <!--  -->
 <template>
   <div class="icons">
-    <div class="icon" v-for="item of iconList" :key="item.id">
-      <div class="icon-img">
-        <img class="icon-img-content" :src="item.iconUrl">
-      </div>
-      <p class="icon-desc">{{item.desc}}</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.iconUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
@@ -20,7 +24,7 @@ export default {
           id: "001",
           iconUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+          desc: "景点门票景点门票"
         },
         {
           id: "002",
@@ -63,53 +67,74 @@ export default {
           iconUrl:
             "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/6b7127c142123eabcf3e41c136e0ccc2.png",
           desc: "京城赏花"
+        },
+        {
+          id: "009",
+          iconUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票"
         }
       ]
     };
+  },
+  computed: {
+    pages: function() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        console.log(pages[page]);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
   }
 };
 </script>
 <style lang='stylus' scoped>
 @import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
 
-.icons {
-  overflow: hidden;
+.icons >>> .swiper-container {
   height: 0;
   padding-bottom: 50%;
+}
 
-  .icon {
-    position: relative;
-    float: left;
-    width: 25%;
-    height: 0;
-    padding-bottom: 25%;
+.icon {
+  position: relative;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
 
-    .icon-img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0.44rem;
-      box-sizing: border-box;
-      padding: 0.1rem;
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
 
-      .icon-img-content {
-        display: block;
-        margin: 0 auto;
-        height: 100%;
-      }
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
     }
+  }
 
-    .icon-desc {
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      height: 0.44rem;
-      line-height: 0.44rem;
-      text-align: center;
-      color: $darkTextColor;
-    }
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    text-align: center;
+    color: $darkTextColor;
+    ellipsis();
   }
 }
 </style>
