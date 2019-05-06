293b9b37aa0ced1d75ccf838e812be4f0106d06f
feinno <guowenhuan_2008@126.com>
2019-05-06 13:37:10 +0800

    周末去哪儿组件

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -4,20 +4,23 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 <script>
 import HomeHeader from "./components/Header";
 import HomeSwiper from "./components/Swiper";
 import HomeIcons from "./components/Icons";
-import HomeRecommend from "./components/Recommend"
+import HomeRecommend from "./components/Recommend";
+import HomeWeekend from "./components/Weekend"
 export default {
   name: "Home",
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 };
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,86 @@
+<!-- 周末游组件 -->
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/24/4b609109dba139.jpg_r_640x214_b28f6052.jpg",
+          title: "宿迁三台山森林公园",
+          desc: "这里最近很火哦，好多人都在点评它呢！"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1811/58/a02ad3ab47e3e3.jpg_r_640x214_08977098.jpg",
+          title: "宿迁三台山森林公园",
+          desc: "这里最近很火哦，好多人都在点评它呢！"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/f1/ae9d4299b8aaea.jpg_r_640x214_0c555905.jpg",
+          title: "宿迁三台山森林公园",
+          desc: "这里最近很火哦，好多人都在点评它呢！"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang='stylus' scoped>
+@import '~styles/mixins.styl';
+
+.title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+
+.item-img-wrapper {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 33.9%;
+
+  .item-img {
+    width: 100%;
+  }
+}
+
+.item-info {
+  padding: 0.1rem;
+
+  .item-title {
+    line-height: 0.54rem;
+    font-size: 0.32rem;
+    ellipsis();
+  }
+
+  .item-desc {
+    line-height: 0.4rem;
+    color: #ccc;
+    ellipsis();
+  }
+}
+</style>
