5209ab5f00647e77df5feb7aa881c74786339835
feinno <guowenhuan_2008@126.com>
2019-05-15 17:05:05 +0800

    完成city初始化

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,44 @@
+<!--  -->
+<template>
+  <div class="header">
+    城市选择
+    <div>
+      <router-link to="/">
+        <div class="iconfont header-back">&#xe624;</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityHeader",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl';
+
+.header {
+  position: relative;
+  overflow: hidden;
+  height: $headerHeight;
+  line-height: 0.86rem;
+  text-align: center;
+  color: #ffffff;
+  background: $bgColor;
+  font-size: 0.32rem;
+
+  .header-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.4rem;
+    color: #ffffff;
+  }
+}
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,48 +6,63 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 <script>
 export default {
   name: "HomeHeader",
-  props:{
-    city:String
+  props: {
+    city: String
   }
 };
 </script>
 <style lang='stylus' scoped>
-@import '~styles/varibles.styl'
-.header 
+@import '~styles/varibles.styl';
+
+.header {
   display: flex;
   line-height: 0.86rem;
   background: $bgColor;
   color: #fff;
-  .header-left 
+
+  .header-left {
     width: 0.64rem;
     float: left;
-    .back-icon 
+
+    .back-icon {
       text-align: center;
       font-size: 0.5rem;
-  .header-input 
+    }
+  }
+
+  .header-input {
     flex: 1;
     height: 0.64rem;
     line-height: 0.64rem;
     margin-top: 0.12rem;
     margin-left: 0.2rem;
-    padding-left:0.2em;
+    padding-left: 0.2em;
     background: #fff;
     border-radius: 0.1rem;
     color: #ccc;
-  .header-right 
+  }
+
+  .header-right {
     width: 1.24rem;
     float: right;
     text-align: center;
-    .arrow-icon
-      margin-left:-0.04rem;
-      font-size:0.24rem;
+    color: #fff;
+
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
 </style>
