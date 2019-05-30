32c6817146f129f84539ecd86a23881928786c39
feinno <guowenhuan_2008@126.com>
2019-05-30 15:22:41 +0800

    完成城市选择搜索功能

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,7 +2,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities='cities'></city-search>
     <city-list :cities='cities' :hotCities='hotCities' :letter='letter'></city-list>
     <city-alphabet :cities='cities' @change="handelLeterChange"></city-alphabet>
   </div>
