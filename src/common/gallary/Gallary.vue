b39a17183478c022437c647bf3f1d228101fcc31
feinno <guowenhuan_2008@126.com>
2019-07-04 14:14:51 +0800

    完成gallary

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container" @click="handelGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(item,index) of imgs" :key="index">
+          <img class="gallary-img" :src="item" />
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonGallary",
+  props: {
+    imgs: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {
+      swiperOption: {
+        //图片显示分页符号
+        pagination: ".swiper-pagination",
+        paginationType: "fraction",
+        //监听父元素变化，自身跟着变化
+        observeParents: true,
+        observer: true
+      }
+    };
+  },
+  methods: {
+    handelGallaryClick: function() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+<style lang='stylus' scoped>
+.container >>> .swiper-container
+  overflow inherit
+.container
+  display flex
+  flex-direction column
+  justify-content center
+  z-index 99
+  position fixed
+  left 0
+  right 0
+  top 0
+  bottom 0
+  background #000
+  .wrapper
+    height 0
+    width 100%
+    padding-bottom 100%
+    .gallary-img
+      width 100%
+    .swiper-pagination
+      color #ffffff
+      bottom -1rem
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,66 +1,78 @@
 <template>
-  <div class="banner">
-    <img
-      class="banner-img"
-      src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg"
-    >
-    <div class="banner-info">
-      <div class="banner-tittle">故宫(AAAAA景区)</div>
-      <div class="banner-number">
-        <span class="iconfont banner-icon">&#xe626;</span>39
+  <div>
+    <div class="banner" @click="handelBannerClick">
+      <img
+        class="banner-img"
+        src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg"
+      />
+      <div class="banner-info">
+        <div class="banner-tittle">故宫(AAAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe626;</span>
+          {{10+10}}
+        </div>
       </div>
     </div>
+    <CommonGallary :imgs="imgs" v-show="showGallary" @close="handelGallaryClose"></CommonGallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from "common/gallary/Gallary";
 export default {
   name: "DeailBanner",
   data() {
-    return {};
+    return {
+      showGallary: false,
+      imgs: [
+        "//imgs.qunarzz.com/p/tts6/1803/43/18bbbac998388902.jpg_r_390x260x90_248d0cfa.jpg",
+        "//imgs.qunarzz.com/p/tts4/1804/68/efa3a4393408f002.jpg_r_390x260x90_43bad027.jpg",
+        "//imgs.qunarzz.com/p/tts1/1804/fd/5fd7733c3802b602.jpg_r_390x260x90_2b630244.jpg"
+      ]
+    };
+  },
+  methods: {
+    handelBannerClick: function() {
+      this.showGallary = true;
+    },
+    handelGallaryClose: function() {
+      this.showGallary = false;
+    }
+  },
+  components: {
+    CommonGallary
   }
 };
 </script>
 <style lang='stylus' scoped>
-.banner {
-  position: relative;
-  overflow: hidden;
-  height: 0;
-  padding-bottom: 55%;
-
-  .banner-img {
-    width: 100%;
-  }
-
-  .banner-info {
-    display: flex;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    line-height: 0.6rem;
-    color: #ffffff;
-    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
-
-    .banner-tittle {
-      flex: 1;
-      font-size: 0.32rem;
-      padding: 0 0.2rem;
-    }
-
-    .banner-number {
-      margin-top: 0.14rem;
-      padding: 0 0.4rem;
-      height: 0.32rem;
-      line-height: 0.32rem;
-      border-radius: 0.2rem;
-      background: rgba(0, 0, 0, 0.8);
-      font-size: 0.24rem;
-
-      .banner-icon {
-        font-size: 0.24rem;
-      }
-    }
-  }
-}
+.banner
+  position relative
+  overflow hidden
+  height 0
+  padding-bottom 55%
+  .banner-img
+    width 100%
+  .banner-info
+    display flex
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    line-height 0.6rem
+    color #ffffff
+    background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-tittle
+      flex 1
+      font-size 0.32rem
+      padding 0 0.2rem
+    .banner-number
+      margin-top 0.14rem
+      padding 0 0.4rem
+      height 0.32rem
+      line-height 0.32rem
+      border-radius 0.2rem
+      background rgba(0, 0, 0, 0.8)
+      font-size 0.24rem
+      .banner-icon
+        font-size 0.24rem
 </style>
